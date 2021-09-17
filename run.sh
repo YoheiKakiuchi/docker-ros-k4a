29e3bc7f4df510f9bb7231783b841cd1ec386355
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2021-09-17 11:35:19 +0900

    update for launch azure_kinect

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ docker run --rm \
        --net=host \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
+       --env="ROS_IP" \
+       --env="ROS_MASTER_URI" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-       -it iory/docker-ros-k4a:latest -i -c \
-       "source /catkin_ws/devel/setup.bash && export ROS_MASTER_URI=http://localhost:11311 && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=720P fps:=30 point_cloud:=false rgb_point_cloud:=false publish_tf:=true color_format:=jpeg use_republish:=false use_depth_registration:=false"
+       -it iory/docker-ros-k4a:latest bash -c "source /catkin_ws/devel/setup.bash && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=720P fps:=30 point_cloud:=false rgb_point_cloud:=false publish_tf:=true color_format:=jpeg use_republish:=true use_depth_registration:=false tf_prefix:=azure_"
 xhost +local:docker
+
